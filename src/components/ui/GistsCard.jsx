978cc61f51e09ce4be08ac680262cab98e4f0981
constayush <constayush@gmail.com>
2025-05-25 12:51:31 +0530

    fixed responsive bugs

--- src/components/ui/GistsCard.jsx
+++ src/components/ui/GistsCard.jsx
@@ -11,9 +11,9 @@ function handleClick() {
 
   return (
 
-    <Link state={props.gist}  to={"/slices"} className="card relative overflow-hidden porject-card min-h-full group max-w-[20rem] min-w-[100%] transition-all bg-[var(--glass-bg-color)] text-[var(--text-color)] group border-2 shadow-2xl border-[#ffffff10] rounded-lg ">
+    <Link state={props.gist}  to={"/slices"} className="card relative grow overflow-hidden porject-card min-h-full group  min-w-[100%] transition-all bg-[var(--glass-bg-color)] text-[var(--text-color)] group border-2 shadow-2xl border-[#ffffff10] rounded-lg ">
       <div className="shine"></div>
-      <span className="flex justify-between">
+      <span className="flex flex-wrap justify-between">
         <h1 className=" text-[1.4rem] opacity-[.9]  font-medium  ">
           # {props.gistName}
         </h1>
