d692307a3334ac22549a860565aaf34104501e4c
constayush <constayush@gmail.com>
2025-08-22 10:22:18 +0530

    made sidebar functional

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,24 +1,33 @@
-
 import ErrorPage from "./components/pages/errorPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Terminal from "./components/pages/Terminal";
 import Hero from "./components/pages/Hero";
 import DinoGame from "./components/pages/Dino";
 import ScrollToTop from "../utils/ScrollToTop";
-import Slices from './components/pages/Slices';
+import Slices from "./components/pages/Slices";
+import Introduction from "./components/slices_components/slices_pages/Introduction";
+import Gallery from "./components/slices_components/slices_pages/Gallery";
+
 function Router() {
   return (
     <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-       <Route path="/" element={<><Hero /></>} />
-       <Route path="/slices" element={<><Slices /></>} />
-       <Route path="/terminal" element={<><Terminal /></>} />
-       <Route path="/orange-rollllllllling" element={<><DinoGame /></>} />
-       <Route path="/*" element={<ErrorPage />} />
-    </Routes>
- </BrowserRouter>
-  )
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        
+        {/* parent route */}
+        <Route path="/slices" element={<Slices />}>
+          {/* nested routes */}
+          <Route path="introduction" element={<Introduction />} />
+          <Route path="gallery" element={<Gallery />} />
+        </Route>
+
+        <Route path="/terminal" element={<Terminal />} />
+        <Route path="/orange-rollllllllling" element={<DinoGame />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default Router
+export default Router;
